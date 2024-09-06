1088aca29044bb20fde1c8537843dc128d7aa0e1
levani-melqadze <l.melqadze2010@gmail.com>
2024-09-06 18:32:10 +0400

    commit

---
+++ level 23/classwork/classwork.py
@@ -0,0 +1,46 @@
+#მომხამრებელს შევეკითხოთ სახელი და იქამდე არ შემოვუშვათ სახლში სანამ ის არიტყვის რომ ქვია სვარჩიკა მაყვალა 
+
+
+name = "svarchika mayva"
+password = "your_password_here"
+
+
+while name != password:
+   
+    password = input("Enter the correct password: ")
+
+print("wellcome house!")
+
+
+
+#მომხმარებელს შეეკითხეთ ექაუნთძე შესასვლელი პაროლი, სანამ ის არ შემოიტანს სწორ პაროლს მას ხელახლა გაუმეორეთ რომ შემოიტანოს პაროლი თუ სწორად შემოიტანს დაბეჭდოს რომ ექაუნთზე შევიდა
+
+name ="levani melqadze"
+password ="enter password"
+
+while name != password:
+
+    password = input("enter the correct password: ")
+
+    print("welcome")
+
+
+
+    #მომხმარებელს შემოატანინეთ რიცხვი შემდეგ while loop ის გამოყენებით 1-დან მომხმარების შემოტანილ რიცხვამდე დაბეჭდეთ მხოლოდ ხუთის ჯერადი რიცხვები
+
+
+number = int(input("Enter number: "))
+
+i = 0
+
+while i <= number:
+    if i % 5 == 0:
+        print("5-ის უმრავლესი რიცხვია")
+    i += 1
+
+
+        
+
+
+
+    
