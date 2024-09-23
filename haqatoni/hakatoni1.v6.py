94ba5e350746dd40a713061acc63157015e94e39
levani-melqadze <l.melqadze2010@gmail.com>
2024-09-23 16:48:26 +0400

    commit

---
+++ haqatoni/hakatoni1.v6.py
@@ -0,0 +1,21 @@
+#"შექმენით number guess თამაში. 
+#description: შექმენით ცვლადი და მიანიჭეთ რაიმე int მნიშვნელობა. შექმენით მეორე ცვლადი რომელშიც მომხმარებელს შემოატანინებთ რაიმე რიცხვს. სანამ არ გამოიცნობს რა რიცხვი იყო პირველ ცვლადში, 
+#დაუწერეთ: ""Try again"" თუ გამოიცნობს მაშინ: ""Congrats. You guessed the number"". (გამოიყენეთ while loop'ი)
+
+number=int(input("enter number :"))
+sum=1
+#while loop
+if number % 5 == 0:
+    print("correct")
+else:
+    print("not correct")
+												
+
+    
+
+
+
+
+
+
+
